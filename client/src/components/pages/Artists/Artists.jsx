0751023d177ceaf448b04dea0896ab7668956d15
Kate Gordon <kategordon.code@gmail.com>
2019-12-09 14:41:16 -0500

    artists page initial commit

---
+++ client/src/components/pages/Artists/Artists.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './artists.css'; 
+const Artists = () => {
+    return (
+        <>
+            <div role="group"className="artistCardContainer"> 
+                <h1>Artist Name</h1>
+                <img alt="artist avatar"src="http://placekitten.com/g/200/300"/> 
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <button>...</button>
+            </div>   
+        </>
+    )
+}
+
+
+export default Artists; 
