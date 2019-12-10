fb8f906d3cefc78fdc35c0b0f90c46557520972e
Puja Fitzpatrick <pujafitz@gmail.com>
2019-12-10 12:54:46 -0500

    Setup react-router, added Home component, started working on Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { ThemeProvider } from '@chakra-ui/core';
+
+import Header from '../src/components/Header';
+import Home from '../src/components/pages/Home';
 import Artists from '../src/components/pages/Artists';
+import Cart from '../src/components/pages/Cart';
 
 function App() {
   return (
     <div className='App'>
       <ThemeProvider>
-        <h1>The Georgia Artists With DisAbilities Gallery</h1>
-        <Artists />
+        <Header />
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/artists'>
+          <Artists />
+        </Route>
+        <Route path='/cart'>
+          <Cart />
+        </Route>
       </ThemeProvider>
     </div>
   );
