7afa8a55cf6e4781ec01626c8060931a2a0dbb17
Kate Gordon <kategordon.code@gmail.com>
2019-12-10 09:34:40 -0500

    styling and setting up page content for Artists

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Artists from '../src/components/pages/Artists'; 
+// import Artists from '../src/components/pages/Artists'; 
+import SingleArtist from '../src/components/pages/SingleArtist'; 
 
 function App() {
   return (
     <div className='App'>
       <h1>The Georgia Artists With DisAbilities Gallery</h1>
-      <Artists/> 
+      {/* <Artists/>  */}
+      <SingleArtist /> 
     </div>
   );
 }
--- client/src/components/pages/Artists/Artists.jsx
+++ client/src/components/pages/Artists/Artists.jsx
@@ -4,12 +4,26 @@ import './artists.css';
 const Artists = () => {
     return (
         <>
-            <div role="group"className="artistCardContainer"> 
-                <h1>Artist Name</h1>
-                <img alt="artist avatar"src="http://placekitten.com/g/200/300"/> 
-                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <button>...</button>
-            </div>   
+         <div role="group" className="artistCards">
+                <div role="group"className="artistCardContainer"> 
+                    <h1>Artist Name</h1>
+                    <img alt="artist avatar"src="http://placekitten.com/g/200/300"/> 
+                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <button>...</button>
+                </div>
+                <div role="group"className="artistCardContainer"> 
+                    <h1>Artist Name</h1>
+                    <img alt="artist avatar"src="http://placekitten.com/g/200/300"/> 
+                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <button>...</button>
+                </div>  
+                <div role="group"className="artistCardContainer"> 
+                    <h1>Artist Name</h1>
+                    <img alt="artist avatar"src="http://placekitten.com/g/200/300"/> 
+                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <button>...</button>
+                </div>   
+            </div>  
         </>
     )
 }
