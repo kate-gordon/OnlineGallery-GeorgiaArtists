bcb5274c68f2b98b8d81b3e19161a5df0ff68a3b
Kate Gordon <kategordon.code@gmail.com>
2019-12-10 10:09:33 -0500

    looking at diff page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-// import Artists from '../src/components/pages/Artists'; 
-import SingleArtist from '../src/components/pages/SingleArtist'; 
+import Artists from '../src/components/pages/Artists'; 
+// import SingleArtist from '../src/components/pages/SingleArtist'; 
 
 function App() {
   return (
     <div className='App'>
       <h1>The Georgia Artists With DisAbilities Gallery</h1>
-      {/* <Artists/>  */}
-      <SingleArtist /> 
+      <Artists/> 
+      {/* <SingleArtist />  */}
     </div>
   );
 }
