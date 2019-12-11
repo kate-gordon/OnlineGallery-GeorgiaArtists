4cb5923ec6f62573ffaaecd82fd5c34abcfc4d98
Puja Fitzpatrick <pujafitz@gmail.com>
2019-12-11 09:30:31 -0500

    Fixed StateProvider bug, fixed Header bug

--- client/src/components/pages/Artists/Artists.jsx
+++ client/src/components/pages/Artists/Artists.jsx
@@ -1,38 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import ArtistCard from '../../Cards/ArtistCard'; 
+import ArtistCard from '../../Cards/ArtistCard';
 
-import axios from 'axios'; 
+import axios from 'axios';
 import './artists.css';
 
 const Artists = () => {
-  const [artists, setArtists] =useState([]); 
+  const [artists, setArtists] = useState([]);
 
   const fetchData = () => {
-    let uri = "http://admin.insae.org/artists/all";
-     axios.get(uri)
-    .then(data => {
-      console.log(data.data)
-      setArtists(data.data); 
-  }).catch ( error => console.log(error))
-}; 
+    let uri = 'http://admin.insae.org/artists/all';
+    axios
+      .get(uri)
+      .then(data => {
+        console.log(data.data);
+        setArtists(data.data);
+      })
+      .catch(error => console.log(error));
+  };
 
   useEffect(() => {
-    const artistData = fetchData(); 
-    console.log(artistData); 
-}, []); 
-
+    const artistData = fetchData();
+    console.log(artistData);
+  }, []);
 
   return (
     <>
-     {artists.map((el, id) => {
-        return (
-        <ArtistCard key={artists[id].id} artist={artists[id]}/>
-        ); 
+      {artists.map((el, id) => {
+        return <ArtistCard key={artists[id].id} artist={artists[id]} />;
       })}
     </>
   );
 };
 
 export default Artists;
-
-
