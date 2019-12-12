7df6140a2dc291abf87e8ccf6f58d63217deb0b8
Puja Fitzpatrick <pujafitz@gmail.com>
2019-12-12 11:31:17 -0500

    Setting up Stripe Checkout Button test code

--- client/src/components/pages/About/About.jsx
+++ client/src/components/pages/About/About.jsx
@@ -6,14 +6,22 @@ const About = () => {
   return (
     <>
       <h1>This is the About Us page.</h1>
-      <h2>Our Mission Statement:</h2>
       <p>
-        To provide mediums through which Georgia artists with dsabilities can
-        display their artistic accomplishments to all disciplines of the arts.
-      </p>
-      <p>
-        To create public awareness of the artistic skills that these artists
-        developed by overcoming the obstacles of a disabvility.
+        Georgia Artists with DisAbilities, Inc. was founded in 1985, with a main
+        goal to identify those disabled citizens of Georgia who are creative,
+        capable, and talented in the arts, so that their achievements and
+        talents can be recognized. There are many disabled artists in Georgia
+        who have visions and dreams, but have no way of making it a reality. We
+        help make it a reality for them.
+        <h2>Our Mission Statement:</h2>
+        <p>
+          To provide mediums through which Georgia artists with dsabilities can
+          display their artistic accomplishments to all disciplines of the arts.
+        </p>
+        <p>
+          To create public awareness of the artistic skills that these artists
+          developed by overcoming the obstacles of a disabvility.
+        </p>
       </p>
     </>
   );
--- client/src/components/pages/Checkout/Checkout.jsx
+++ client/src/components/pages/Checkout/Checkout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import StripeButton from '../../Stripe/StripeButton';
 
 const Checkout = () => {
   return (
     <div>
       <h1>Checkout page test.</h1>
+      <StripeButton />
     </div>
   );
 };
