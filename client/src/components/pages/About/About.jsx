d150f21e5cf730ebb63e4706c80982a134f8da2f
Puja Fitzpatrick <pujafitz@gmail.com>
2019-12-17 11:15:12 -0500

    Commit before attempting hotkey v3 stripe form

--- client/src/components/pages/About/About.jsx
+++ client/src/components/pages/About/About.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import { Box, Grid } from "@chakra-ui/core";
-import "./about.css";
+import { Box, Grid } from '@chakra-ui/core';
+import './about.css';
 
 const About = () => {
   return (
@@ -9,12 +9,12 @@ const About = () => {
       <Grid templateColumns='repeat(2, 1fr)' gap={6} height='100vh'>
         <Box p={4}>
           <h1>Georgia Artists with DisAbilities</h1>
-          Georgia Artists with DisAbilities, Inc. was founded in 1985, with a
-          main goal to identify those disabled citizens of Georgia who are
-          creative, capable, and talented in the arts, so that their
-          achievements and talents can be recognized. There are many disabled
-          artists in Georgia who have visions and dreams, but have no way of
-          making it a reality. We help make it a reality for them.
+          Georgia Artists with DisAbilities was founded in 1985, with a main
+          goal to identify those disabled citizens of Georgia who are creative,
+          capable, and talented in the arts, so that their achievements and
+          talents can be recognized. There are many disabled artists in Georgia
+          who have visions and dreams, but have no way of making it a reality.
+          We help make it a reality for them.
         </Box>
         <Box p={4}>
           <h2>Our Mission Statement:</h2>
@@ -25,7 +25,7 @@ const About = () => {
           </p>
           <p>
             To create public awareness of the artistic skills that these artists
-            developed by overcoming the obstacles of a disabvility.
+            developed by overcoming the obstacles of a disability.
           </p>
         </Box>
       </Grid>
