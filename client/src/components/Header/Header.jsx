de6284b4628049ee158e437c0f2805faad837bbc
Kate Gordon <kategordon.code@gmail.com>
2019-12-17 10:58:57 -0500

    Header Grid refactor

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,42 +1,40 @@
-import React from 'react';
-import { Box, Button, Image } from '@chakra-ui/core';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Box, Button, Grid, Image } from '@chakra-ui/core'
+import { Link } from 'react-router-dom'
 
-import logo from './logo.png';
-import Navbar from './Navbar/Navbar';
-import { MdShoppingCart } from 'react-icons/md';
+import logo from './logo.png'
+import Navbar from './Navbar/Navbar'
+import { MdShoppingCart } from 'react-icons/md'
 
-import './header.css';
+import './header.css'
 
 const Header = () => {
   return (
-    <div className='header'>
-      <div className='headerContent'>
-        <Box>
-          <Link to='/' className='orgName'>
-            Georgia Artists with DisAbilities
-          </Link>
-        </Box>
-        <Box>
-          <Image
-            src={logo}
-            alt='artist palette with colorful paint logo'
-            width='250px'
-            objectFit='contain'
-          />
-        </Box>
-        <Box>
-          <Link to='/cart' className='cartButton'>
-            <Button size='xs'>
-              <MdShoppingCart />
-              Cart
-            </Button>
-          </Link>
-        </Box>
-      </div>
-      <Navbar />
-    </div>
-  );
-};
+    <Grid templateColumns='repeat(3, 10% 80% 10%)' gap={5}>
+      <Box>
+        <Link to='/' className='orgName'>
+          Georgia Artists with DisAbilities
+        </Link>
+      </Box>
+      <Box>
+        <Image
+          src={logo}
+          alt='artist palette with colorful paint logo'
+          width='200px'
+          objectFit='contain'
+        />
+        <Navbar />
+      </Box>
+      <Box>
+        <Link to='/cart' className='cartButton'>
+          <Button size='xs'>
+            <MdShoppingCart />
+            Cart
+          </Button>
+        </Link>
+      </Box>
+    </Grid>
+  )
+}
 
-export default Header;
+export default Header
