6f66745aa80518ba6ce9cc668018a1ea74d655cd
Puja Fitzpatrick <pujafitz@gmail.com>
2019-12-19 03:25:27 -0500

    Working on CartCard without breaking the cart

--- client/src/components/Cards/CartCard.jsx
+++ client/src/components/Cards/CartCard.jsx
@@ -2,9 +2,20 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { StateContext } from '../../context';
 
-import { Box, Button, Image, IconButton } from '@chakra-ui/core';
+import {
+  Badge,
+  Box,
+  Button,
+  Image,
+  IconButton,
+  Stack,
+  Text
+} from '@chakra-ui/core';
 
 const CartCard = props => {
+  const [{ cart }] = useContext(StateContext);
+  let cartArray = [];
+  cart.map(item => cartArray.push(item.artwork_id));
   const { piece } = props;
   const [value, dispatch] = useContext(StateContext);
   const handleClick = e => {
@@ -14,17 +25,51 @@ const CartCard = props => {
     });
   };
 
+  const removeFromCartBox = (
+    <Badge
+      color='red.600'
+      style={{
+        color: 'gray.900',
+        backgroundColor: 'gray.100',
+        fontSize: '1.2em',
+        padding: '20px'
+      }}
+      _hover={{ color: 'gray.900', backgroundColor: 'gray.100' }}
+    ></Badge>
+  );
   return (
     <>
-      {piece.title}
       <IconButton
         className='iconBtn'
         onClick={handleClick}
         aria-label='Remove from Cart'
         size='xs'
-        icon='add'
+        icon='minus'
         value={piece}
       />
+
+      <Box
+        d='flex'
+        flexDirection='column'
+        alignItems='flex-start'
+        justifyContent='flex-start'
+      >
+        <Stack spacing={2}>
+          <Text fontSize='3xl' fontFamily='fira-sans'>
+            {piece.title}
+          </Text>
+          <Text fontSize='2xl' fontFamily='fira-sans'>
+            By: {piece.firstname} {piece.lastname}
+          </Text>
+        </Stack>
+        <Image
+          htmlWidth='100%'
+          htmlHeight='auto'
+          src={piece.picture}
+          alt={piece.title}
+          boxShadow='4px 4px 2px 2px grey'
+        />
+      </Box>
     </>
   );
 };
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,48 +1,48 @@
-import React from "react";
-import { Box, Grid } from "@chakra-ui/core";
-import { MdFavorite } from "react-icons/md";
-import { GoMarkGithub } from "react-icons/go";
+import React from 'react';
+import { Box, Grid } from '@chakra-ui/core';
+import { MdFavorite } from 'react-icons/md';
+import { GoMarkGithub } from 'react-icons/go';
 
 const Footer = () => {
   return (
     <>
       <Grid
-        borderTop="solid"
-        className="header"
-        templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(4, 1fr)"]}
+        borderTop='solid'
+        className='header'
+        templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(4, 1fr)']}
         m={4}
         p={4}
       >
-        <Box d="flex" alignItems="center" justifyContent="center">
-          Built with <Box as={MdFavorite} /> by
+        <Box d='flex' alignItems='center' justifyContent='center'>
+          Built with {'  '} <Box as={MdFavorite} /> {'  '} by
         </Box>
-        <Box d="flex" alignItems="center" justifyContent="center">
+        <Box d='flex' alignItems='center' justifyContent='center'>
           <a
-            style={{ color: "black", textDecoration: "none" }}
-            target="_blank"
-            href="https://github.com/pujafitz"
+            style={{ color: 'black', textDecoration: 'none' }}
+            target='_blank'
+            href='https://github.com/pujafitz'
           >
-            Puja Fitzpatrick
+            Puja Fitzpatrick {'  '}
             <Box as={GoMarkGithub} />
           </a>
         </Box>
-        <Box d="flex" alignItems="center" justifyContent="center">
+        <Box p={1} d='flex' alignItems='center' justifyContent='center'>
           <a
-            style={{ color: "black", textDecoration: "none" }}
-            target="_blank"
-            href="https://github.com/cmkemp52"
+            style={{ color: 'black', textDecoration: 'none' }}
+            target='_blank'
+            href='https://github.com/kate-gordon'
           >
-            Christopher Kemp
+            Kate Gordon {'  '}
             <Box as={GoMarkGithub} />
           </a>
         </Box>
-        <Box p={1} d="flex" alignItems="center" justifyContent="center">
+        <Box d='flex' alignItems='center' justifyContent='center'>
           <a
-            style={{ color: "black", textDecoration: "none" }}
-            target="_blank"
-            href="https://github.com/kate-gordon"
+            style={{ color: 'black', textDecoration: 'none' }}
+            target='_blank'
+            href='https://github.com/cmkemp52'
           >
-            Kate Gordon
+            Christopher Kemp {'  '}
             <Box as={GoMarkGithub} />
           </a>
         </Box>
