99f336fed8c8b0d8a14b1ac18933e93e22191bbd
Christopher Kemp <cmkemp52@gmail.com>
2019-12-19 10:50:52 -0500

    added pagination

--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -1,29 +1,79 @@
-import React from "react";
+import React, { Component } from "react";
 import ArtPieceCard from "../../Cards/ArtPieceCard";
+import ReactPaginate from "react-paginate";
 
 import { Grid } from "@chakra-ui/core";
+import "./home.css";
 
-const Home = props => {
-  const { artPieces } = props;
+export class GalleryList extends Component {
+  render() {
+    let artworkNodes = this.props.data.map(function(artwork, index) {
+      return <ArtPieceCard key={index} piece={artwork} />;
+    });
 
-  return (
-    <>
-      <Grid
-        templateColumns={[
-          "repeat(1, 1fr)",
-          "repeat(1, 1fr)",
-          "repeat(2, 1fr)",
-          "repeat(3, 1fr)"
-        ]}
-        mt={4}
-        gap={6}
-      >
-        {artPieces.map((artpiece, id) => {
-          return <ArtPieceCard key={artPieces[id].id} piece={artPieces[id]} />;
-        })}
-      </Grid>
-    </>
-  );
-};
+    return <> {artworkNodes} </>;
+  }
+}
+
+class Home extends Component {
+  state = {
+    data: [],
+    offset: 0,
+    pageCount: 0
+  };
+  componentDidMount() {
+    this.setArtPage();
+  }
+  setArtPage() {
+    this.setState({
+      data: this.props.artPieces.slice(
+        this.state.offset,
+        this.state.offset + 6
+      ),
+      pageCount: Math.ceil(this.props.artPieces.length / 6)
+    });
+  }
+  handlePageClick = data => {
+    console.log("hi", data);
+    let selected = data.selected;
+    let offset = Math.ceil(selected * 6);
+
+    this.setState({ offset: offset }, () => {
+      this.setArtPage();
+    });
+  };
+  render() {
+    return (
+      <>
+        <Grid
+          templateColumns={[
+            "repeat(1, 1fr)",
+            "repeat(1, 1fr)",
+            "repeat(2, 1fr)",
+            "repeat(3, 1fr)"
+          ]}
+          mt={4}
+          gap={6}
+        >
+          <GalleryList data={this.state.data} />
+        </Grid>
+        <ReactPaginate
+          previousLabel={"previous"}
+          nextLabel={"next"}
+          breakLabel={"..."}
+          breakClassName={"break-me"}
+          pageCount={this.state.pageCount}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={3}
+          onPageChange={this.handlePageClick}
+          containerClassName={"pagination"}
+          pageLinkClassName={"pages"}
+          activeClassName={"activePage"}
+          activeLinkClassName={"activeLink"}
+        />
+      </>
+    );
+  }
+}
 
 export default Home;
